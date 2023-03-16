f07a129dcf0a32f66572bbcb727164d447832b54
@S52000043 <52000043@student.tdtu.edu.vn>
2023-03-16 08:37:11 +0700

    chore: hash password

--- db/user.js
+++ db/user.js
@@ -1,11 +1,13 @@
 const db = require('./dbConnect');
 const mysql = require('mysql');
+const bcript = require('bcrypt');
 
 const register = async (username, password, callback) => {
   let success = false;
+  const hashedPassword = await bcript.hash(password, 10);
   db.getConnection(async (err, connection) => {
     const sql = 'INSERT INTO user VALUES (?, ?)';
-    const query = mysql.format(sql, [username, password]);
+    const query = mysql.format(sql, [username, hashedPassword]);
 
     await connection.query(query, (err, result) => {
       success = true;
