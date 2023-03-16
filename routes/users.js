78fb2fcb3a8d8e405a4f30166e2378890d6efc36
@S52000049 <52000049@student.tdtu.edu.vn>
2023-03-16 19:08:33 +0700

    chore: update user api route

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const {
   updateUserById,
   deleteUserById,
 } = require('../db/user');
+const { body } = require('express-validator');
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
@@ -17,10 +18,19 @@ router.get('/:id', async (req, res, next) => {
   res.json(result);
 });
 
-router.put('/:id', async (req, res, next) => {
-  const result = await updateUserById(req.params.id, req.body);
-  res.json(result);
-});
+router.put(
+  '/:id',
+  body('email').notEmpty().isEmail().normalizeEmail(),
+  body('username').notEmpty(),
+  body('password').notEmpty().isLength({ min: 6, max: 20 }),
+  body('address').notEmpty(),
+  body('phone').notEmpty().isNumeric(),
+  body('gender').notEmpty().isIn([0, 1]),
+  async (req, res, next) => {
+    const result = await updateUserById(req.params.id, req.body);
+    res.json(result);
+  }
+);
 
 router.delete('/:id', async (req, res, next) => {
   const result = await deleteUserById(req.params.id);
