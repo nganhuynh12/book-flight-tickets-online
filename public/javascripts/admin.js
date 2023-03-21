b07164b6520f4f77fd943569e9340f8230511d57
@S52000086 <52000086@student.tdtu.edu.vn>
2023-03-21 18:32:08 +0700

    add,view,edit admin

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -6,3 +6,46 @@ function openTabs(tabName) {
     }
     document.getElementById(tabName).style.display = "block";  
 }
+
+
+
+/* Thêm chuyến bay */
+function showAddFlightForm() {
+  document.getElementById("add-flight-form").classList.add("show");
+  setTimeout(function() {
+    document.querySelector("#add-flight-form form").classList.add("show");
+  }, 50);
+}
+
+function hideAddFlightForm() {
+  document.querySelector("#add-flight-form form").classList.remove("show");
+  setTimeout(function() {
+    document.getElementById("add-flight-form").classList.remove("show");
+  }, 300);
+}
+
+
+/* Xem thông tin chi tiết chuyến bay */
+function showFlightDetails() {
+  let dialog = document.getElementById("flight-details").querySelector('.dialog');
+  dialog.classList.add("show");
+  setTimeout(function() {
+    dialog.focus();
+  }, 6000); // focus on dialog after animation completes
+  document.getElementById("flight-details").classList.add("show");
+}
+
+function hideFlightDetails() {
+  let dialog = document.getElementById("flight-details").querySelector('.dialog');
+  dialog.classList.remove("show");
+  document.getElementById("flight-details").classList.remove("show");
+}
+
+
+
+  /* Chỉnh sửa thông tin chuyến bay */
+
+
+
+
+
