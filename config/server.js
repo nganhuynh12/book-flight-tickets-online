1147a6b13a35f97da1aefc4678dc4239f06169ba
@S52000049 <52000049@student.tdtu.edu.vn>
2023-03-25 21:54:24 +0700

    chore: seperate global midlle ware config

---
+++ config/server.js
@@ -0,0 +1,74 @@
+const { engine } = require('express-handlebars');
+const { default: helmet } = require('helmet');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const passport = require('passport');
+const session = require('express-session');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const bundler = require('connect-bundle')(require('./config'));
+const express = require('express');
+
+module.exports = (app) => {
+  // view engine setup
+  app.engine(
+    'hbs',
+    engine({
+      extname: 'hbs',
+      helpers: {
+        gen_seats(colNum, rowNum, options) {
+          const matrix = [];
+          let count = 1;
+          for (let i = 0; i < rowNum; i++) {
+            const row = [];
+            for (let j = 0; j < colNum; j++) {
+              row.push(
+                `<div style="width: 50px; height: 50px;">${options.fn(
+                  count
+                )}</div>`
+              );
+              count += 1;
+            }
+            matrix.push(`<div style="display: flex">${row.join(' ')}</div>`);
+          }
+          return `<div style='display: flex; flex-direction: column;'> 
+              ${matrix.join(' ')}
+          </div>
+        `;
+        },
+        static(name) {
+          const baseUrl = '';
+
+          return baseUrl + name;
+        },
+      },
+    })
+  );
+  app.set('view engine', 'hbs');
+  app.use(bundler);
+
+  app.use(logger('dev'));
+  app.use(helmet());
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeader: true,
+    legacyHeader: false,
+  });
+  app.use(limiter);
+  app.use(
+    session({
+      secret: 'suvasdhfioweufklj',
+      resave: true,
+      saveUninitialized: true,
+    })
+  );
+  app.use(passport.session());
+  app.use(passport.authenticate('session'));
+};
