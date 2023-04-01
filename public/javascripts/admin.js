0f80f83ac7abfa083d2daea329a5500388fe6c6a
@S52000277 <52000277@student.tdtu.edu.vn>
2023-04-02 01:56:30 +0700

    edit admin

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -3,8 +3,14 @@ $(document).ready(() => {
   const btnHideForm = $('.form-close');
   const prefixList = ['sidebar', 'Opentab'];
   const btnUpdate = $('.btn-edit');
-  const showFlightForm = $('#add-flight-form');
+  const addFlightForm = $('#add-flight-form');
+  const updateFlightForm = $('#update-flight-form');
+  const btnViewFlightDetail = $('.btn-view');
+  const btnDelete = $('.btn-delete');
+  const btnCloseDialog = $('.btn-close-dialog');
+  const btnAddPlace = $('#btn-add-place');
   let formName = '';
+  let dialogID = '';
 
   prefixList.forEach((prefix) => {
     const elementList = $(`div[id^=${prefix}]`);
@@ -28,9 +34,8 @@ $(document).ready(() => {
           return `<td>${data[field]}</td>`;
         })}
           <td>
-             <button class='btn-view'><span class='fa fa-eye'></span></button>
              <button class='btn-edit'><span class='fa fa-pencil'></span></button>
-             <button class='btn-delete'><span
+             <button class='btn-delete' id='btn-delete-place'><span
                class='fa fa-trash'
             ></span></button>
            </td>
@@ -89,30 +94,47 @@ $(document).ready(() => {
   };
 
   /* Thêm chuyến bay */
-  btnAddFlight.on('click', function(){
-    var id = $(this).attr('id');
-    formName = '#' + id.slice(id.indexOf('-') + 1, id.length) + '-form';
-    showAddForm();
+  btnAddFlight.on('click', showAddFlightForm);
+  btnHideForm.on('click', function(){
+    hideForm();
+    hideDialog();
   });
-  btnHideForm.on('click', hideForm);
-
-  btnUpdate.on("click",function(){
+  btnUpdate.on("click", function(){
+    console.log($(this).attr('id'));
+    showUpdateFlightForm();
+  });
+  btnViewFlightDetail.on("click",function(){
+    showDialog();
+  })
+  btnDelete.on("click",function(){
     var id = $(this).attr('id');
-    formName = '#' + id.slice(id.indexOf('-') + 1, id.length) + '-form';
-    showUpdateForm();
+    dialogID = id.slice(id.indexOf('-') + 1, id.length);
+    var currentRow = $(this).closest("tr");
+    var idFlight = currentRow.find("td:eq(0)").html();
+    $('#flightID').html(idFlight);
+    showDialog();
+  })
+  btnCloseDialog.on("click",hideDialog);
+  btnAddPlace.on("click",function(){
+    var id = btnAddPlace.attr('id')
+    dialogID = id.slice(id.indexOf('-') + 1, id.length);
+    showDialog();
   })
 
-  function showAddForm() {
-    showFlightForm.css("display","block");
+
+  function showAddFlightForm() {
+    formName = '#' + addFlightForm.attr('id');
+    addFlightForm.css("display","block");
     setTimeout(function () {
       document.querySelector('#add-flight-form form').classList.add('show');
     }, 50);
   };
 
-  function showUpdateForm() {
-    $(formName).css("display","block");
+  function showUpdateFlightForm() {
+    formName = '#' + updateFlightForm.attr('id');
+    updateFlightForm.css("display","block");
     setTimeout(function () {
-      document.querySelector(formName + ' form').classList.add('show');
+      document.querySelector('#update-flight-form form').classList.add('show');
     }, 50);
   };
 
@@ -133,23 +155,23 @@ $(document).ready(() => {
   })
 
   /* Xem thông tin chi tiết chuyến bay */
-  const showFlightDetails = () => {
+  function showDialog() {
     let dialog = document
-      .getElementById('flight-details')
+      .getElementById(dialogID)
       .querySelector('.dialog');
     dialog.classList.add('show');
     setTimeout(function () {
       dialog.focus();
     }, 6000); // focus on dialog after animation completes
-    document.getElementById('flight-details').classList.add('show');
+    document.getElementById(dialogID).classList.add('show');
   };
 
-  const hideFlightDetails = () => {
+  function hideDialog() {
     let dialog = document
-      .getElementById('flight-details')
+      .getElementById(dialogID)
       .querySelector('.dialog');
     dialog.classList.remove('show');
-    document.getElementById('flight-details').classList.remove('show');
+    document.getElementById(dialogID).classList.remove('show');
   };
 
   /* Chỉnh sửa thông tin chuyến bay */
