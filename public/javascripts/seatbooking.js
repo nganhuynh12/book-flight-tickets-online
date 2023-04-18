ce7c066ded876b5f289d70281efec1dad63b92ea
@S52000049 <52000049@student.tdtu.edu.vn>
2023-04-18 11:50:05 +0700

    feat: seat booking page

---
+++ public/javascripts/seatbooking.js
@@ -0,0 +1,33 @@
+$(document).ready(() => {
+  const container1 = $('.container1');
+  const seats = $('.row .seat:not(.occupied)');
+  let seatList = [];
+  const numSeat = 80;
+
+  for (let i = 0; i < numSeat / 8; i++) {
+    let temp = [];
+    for (let j = 0; j < 8; j++) {
+      temp.push(
+        $(`<div class="seat" id=${i * 8 + j + 1}></div>`).on(
+          'click',
+          (event) => {
+            if (
+              event.target.classList.contains('seat') &&
+              !event.target.classList.contains('occupied')
+            ) {
+              if (!event.target.classList.contains('selected')) {
+                seatList.push(event.target.id);
+              } else {
+                seatList = seatList.filter((seat) => seat !== event.target.id);
+              }
+              event.target.classList.toggle('selected');
+            }
+            console.log(seatList);
+          }
+        )
+      );
+    }
+
+    container1.append($(`<div class="row1"></div>`).append(temp));
+  }
+});
