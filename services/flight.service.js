a81ec7262f0fb6ef5d7a09bdb98e9cd25f3ab0da
@S52000049 <52000049@student.tdtu.edu.vn>
2023-04-18 11:47:26 +0700

    fix: flight with location data

--- services/flight.service.js
+++ services/flight.service.js
@@ -1,3 +1,15 @@
 const baseService = require('./base.service');
+const Location = require('../models/index').locations;
 
-module.exports = class flightService extends baseService {};
+module.exports = class flightService extends baseService {
+  findAllWithLocationData(option) {
+    console.log(option.where);
+    return this.model.findAll({
+      include: [
+        { model: Location, as: 'startLocation' },
+        { model: Location, as: 'arriveLocation' },
+      ],
+      ...option,
+    });
+  }
+};
