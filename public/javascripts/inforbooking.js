8be0aa63feef949d862247bbcee13d1db7ad6618
@S52000049 <52000049@student.tdtu.edu.vn>
2023-04-20 15:02:17 +0700

    feat: infor booking page logic

--- public/javascripts/inforbooking.js
+++ public/javascripts/inforbooking.js
@@ -1,7 +1,35 @@
 $(document).ready(() => {
   const ticketData = JSON.parse(window.localStorage.getItem('ticketData'));
   const priceSpan = $('.price');
+  const basePriceSpan = $('#basePrice');
+  const luggagePriceSpan = $('#luggagePrice');
+  const seatPriceSpan = $('#seatPrice');
+  const modalTitleSpan = $('h5.modal-title');
   console.log(ticketData);
+
+  basePriceSpan.text(
+    Intl.NumberFormat('VND', { style: 'currency', currency: 'VND' }).format(
+      ticketData.basePrice
+    )
+  );
+
+  luggagePriceSpan.text(
+    Intl.NumberFormat('VND', { style: 'currency', currency: 'VND' }).format(
+      ticketData.luggagePrice
+    )
+  );
+
+  seatPriceSpan.text(
+    Intl.NumberFormat('VND', { style: 'currency', currency: 'VND' }).format(
+      ticketData.seatPrice
+    )
+  );
+
+  modalTitleSpan.text(
+    `Chuyến bay từ ${ticketData.flight.startLocation.value} đến ${
+      ticketData.flight.arriveLocation.value
+    } - ${new Date(ticketData.flight.startTime).toUTCString()}`
+  );
 
   const ticketPriceText = Intl.NumberFormat('VND', {
     style: 'currency',
